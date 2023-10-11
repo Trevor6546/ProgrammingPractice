11fd65a3a042b49afaad50bf57b33dd92acfdf1c
Trevor6546 <57647629+Trevor6546@users.noreply.github.com>
2023-10-10 17:21:50 -0700

    Add files via upload

---
+++ codeforces/1873A-Short_Sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s == "bca" || s == "cab"){
+            cout <<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+}
