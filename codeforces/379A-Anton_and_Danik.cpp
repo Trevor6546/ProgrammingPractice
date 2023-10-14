4829e604ddccce02a7abe63e52e58afcf8ee1799
Trevor6546 <57647629+Trevor6546@users.noreply.github.com>
2023-10-14 16:37:54 -0700

    Add files via upload

--- codeforces/379A-Anton_and_Danik.cpp
+++ codeforces/379A-Anton_and_Danik.cpp
@@ -2,27 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;
-    cin>>t;
-    string s;
-    cin>>s;
-    int dcnt = 0;
-    int acnt = 0;
-    for(int i=0; i<t;i++){
-        if(s[i] == 'A'){
-            acnt++;
+    int x,y;
+    cin>>x>>y;
+    int width = 0;
+    for(int i=0; i<x;i++){
+        int z;
+        cin>>z;
+        if(z > y){
+            width += 2;
         }
         else{
-            dcnt++;
+            width++;
         }
     }
-    if(acnt > dcnt){
-        cout << "Anton" << endl;
-    }
-    else if(acnt == dcnt){
-        cout << "Friendship" << endl;
-    }
-    else{
-        cout << "Danik" << endl;
-    }
+    cout<<width<<endl;
 }
