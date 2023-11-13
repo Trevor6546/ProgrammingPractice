2af056cc8e39e9ed5508891e3a659c18ee401ea0
Trevor6546 <57647629+Trevor6546@users.noreply.github.com>
2023-11-13 12:28:42 -0800

    Add files via upload

---
+++ codeforces/883A-Cut_The_Rope.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int count = 0;
+        while(n--){
+            int a,b;
+            cin>>a>>b;
+            if((a-b)>0){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+}
